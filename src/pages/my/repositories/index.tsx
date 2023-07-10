f176cd570ce9a663bab985cc60ceb24c0017291d
Khusamov Sukhrob <khusamov@yandex.ru>
2023-07-10 21:28:46 +0300

    Обновлен код вывода списка репозиторий

--- src/pages/my/repositories/index.tsx
+++ src/pages/my/repositories/index.tsx
@@ -3,29 +3,40 @@ import useFetch from 'use-http'
 import Layout from '@theme/Layout';
 import styles from './index.module.scss';
 
+const repoListAddress = 'https://api.github.com/users/khusamov/repos?per_page=100'
+
 export default function() {
-	const {loading, error, data = []} = useFetch('https://api.github.com/users/khusamov/repos?per_page=100', {}, [])
+	const {loading, error, data = []} = useFetch(repoListAddress, {}, [])
 	return (
 		<Layout>
 			<div className={styles.layout}>
-				{error && 'Error!'}
-				{loading && 'Loading...'}
-				<table>
-					<tr>
-						<th>Репозиторий</th>
-						<th>Описание</th>
-					</tr>
-					{data.map(item => (
-						<tr>
-							<td>
-								<a href={`https://github.com/khusamov/${item.name}`} target='_blank'>
-									{item.name}
-								</a>
-							</td>
-							<td>{item.description}</td>
-						</tr>
-					))}
-				</table>
+				<h1>Мои публичные репозитории</h1>
+				{error && 'Ошибка доступа к серверу: ' + error.message}
+				{loading && 'Загружается список репозиториев...'}
+				{!loading && (
+					<table>
+						<tbody>
+							<tr>
+								<th>Репозиторий</th>
+								<th>Описание</th>
+							</tr>
+							{
+								data.map(
+									(item: any) => (
+										<tr>
+											<td>
+												<a href={`https://github.com/khusamov/${item.name}`} target='_blank'>
+													{item.name}
+												</a>
+											</td>
+											<td>{item.description}</td>
+										</tr>
+									)
+								)
+							}
+						</tbody>
+					</table>
+				)}
 			</div>
 		</Layout>
 	)
